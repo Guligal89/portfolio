0b923b31539ef16a3c2183885b4e2900937e9188
Gabriel <grassia.gabriel@gmail.com>
2023-02-07 18:18:25 +0100

    Fixed contact button text bug

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,6 @@
 hamburguer = document.getElementsByClassName('hamburguer');
 main = document.getElementsByTagName("main")[0];
-mobileNav = document.getElementById("mobile-nav")
+mobileNav = document.getElementById("mobile-nav");
 let activeNav = false;
 
 function toggleNav() {
